3b0089887ec85f2ae170271e5618286c95c9f3f6
Rayssa Gomes <rayssacgomess@gmail.com>
2024-04-21 13:03:24 -0300

    Update favicon

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,13 @@
 const brandName = `Matemachica`;
 const copyright = `Todos os direitos reservados.`;
+
+addFavicon('/public/cute-hamster.png');
+
+// favicon
+function addFavicon(url) {
+    var link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'icon';
+    link.href = url;
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
